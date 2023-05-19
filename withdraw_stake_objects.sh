66086118f82d13fdeb1b350eaa6412db3eb67194
Markkus Millend <mm@liangroup.io>
2023-05-19 16:32:49 +0200

    Add script for merging all staked SUI objects

--- withdraw_stake_objects.sh
+++ withdraw_stake_objects.sh
@@ -11,7 +11,7 @@ stake_objects=$(curl -s -X POST -H "Content-Type: application/json" -d '{
 }' https://rpc.mainnet.sui.io/ | jq -r '.result | to_entries[] | select(.value.validatorAddress == "'${VALIDATOR_ADDRESS}'") | .value.stakes')
 
 # Withdraw all staked SUI objects, one by one.
-echo "${stake_objects}" | jq -r '.[] | .stakedSuiId' | while read stakedSuiId; do
-    echo "Withdrawing staked SUI object: ${stakedSuiId}"
-    sui client call --package 0x3 --module sui_system --function request_withdraw_stake --args 0x5 "${stakedSuiId}" --gas-budget 20000000
+echo "${stake_objects}" | jq -r '.[] | .stakedSuiId' | while read staked_sui_id; do
+    echo "Withdrawing staked SUI object: ${staked_sui_id}"
+    sui client call --package 0x3 --module sui_system --function request_withdraw_stake --args 0x5 "${staked_sui_id}" --gas-budget 20000000
 done
