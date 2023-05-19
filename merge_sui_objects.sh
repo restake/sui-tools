39352826a97f216031c8f8e848dd87455285e347
Markkus Millend <mm@liangroup.io>
2023-05-19 19:23:59 +0200

    Merge Sui coins instead of StakedSui coins

---
+++ merge_sui_objects.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+sui_objects=$(curl -s -X POST -H "Content-Type: application/json" -d '{
+    "method": "suix_getOwnedObjects",
+    "jsonrpc": "2.0",
+    "id": "1",
+    "params": [
+        "'${VALIDATOR_ADDRESS}'",
+        {
+            "filter": {
+                "StructType": "0x2::coin::Coin<0x2::sui::SUI>"
+            }
+        }
+    ]
+}' https://rpc.mainnet.sui.io/ | jq -r '.result.data')
+
+echo $sui_objects | jq
+
+first_sui_object=$(echo "${sui_objects}" | jq -r '.[0] | .data.objectId')
+echo "Merging SUI objects to: ${first_sui_object}"
+
+# Merge all SUI coin objects into the first one.
+# TODO: this can actually be executed with a single transaction block.
+echo "${sui_objects}" | jq -r '.[1:] | .[] | .data.objectId' | while read sui_object_id; do
+    echo "Merging staked SUI object: ${sui_object_id}"
+    sui client merge-coin --primary-coin "${first_sui_object}" --coin-to-merge "${sui_object_id}" --gas-budget 20000000
+done
